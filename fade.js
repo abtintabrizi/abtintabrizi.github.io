a7f6bd1f10ba1cc1953f2a1fef7f393c64cb17da
STr3AK1337 <tabrizi2002@yahoo.com>
2021-07-19 14:39:07 -0400

    General responsiveness updates

--- fade.js
+++ fade.js
@@ -11,4 +11,12 @@ $(document).ready(function () {
             }
         });
     });
+    $('.fade').each(function (i) {
+        var bottom_of_object = $(this).position().top + ($(this).outerHeight()) / 5;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if (bottom_of_window > bottom_of_object) {
+            $(this).animate({ 'opacity': '1' }, 1000);
+        }
+    });
 });
