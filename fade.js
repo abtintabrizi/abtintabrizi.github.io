7cdc8f32d43d4daf16c5c0fa4c31c4a49cc2fd6e
STr3AK1337 <tabrizi2002@yahoo.com>
2021-07-18 21:34:57 -0400

    Update fade.js

--- fade.js
+++ fade.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $(window).scroll(function () {
 
         $('.fade').each(function (i) {
-            var bottom_of_object = $(this).position().top + ($(this).outerHeight()) / 4;
+            var bottom_of_object = $(this).position().top + ($(this).outerHeight()) / 5;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             if (bottom_of_window > bottom_of_object) {
