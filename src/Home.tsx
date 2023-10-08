a48929cc1281044ca6001b2cb64f98a98e83ece3
Abtin Tabrizi <aaghazad@uwaterloo.ca>
2023-10-07 22:18:14 -0400

    Initial tailwind integration

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,3 +1,69 @@
+import {
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  MobileNav,
+  Navbar,
+  Typography,
+} from '@material-tailwind/react';
+import { useState } from 'react';
+
 export default function Home() {
-  return <div>test</div>;
+  const [open, setOpen] = useState(false);
+
+  const openDrawer = () => setOpen(true);
+  const closeDrawer = () => setOpen(false);
+  const [openNav, setOpenNav] = useState(false);
+
+  const navList = (
+    <ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <a href='#' className='flex items-center'>
+          Pages
+        </a>
+      </Typography>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <a href='#' className='flex items-center'>
+          Account
+        </a>
+      </Typography>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <a href='#' className='flex items-center'>
+          Blocks
+        </a>
+      </Typography>
+      <Typography
+        as='li'
+        variant='small'
+        color='blue-gray'
+        className='p-1 font-normal'
+      >
+        <a href='#' className='flex items-center'>
+          Docs
+        </a>
+      </Typography>
+    </ul>
+  );
+  return (
+    <div className='bg-gradient-to-br from-white to-light-blue-100 w-screen h-screen'>
+      <main className='flex flex-col justify-center items-center'>test</main>
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,58 @@
 import ReactDOM from 'react-dom/client';
 import Home from './Home';
+import './index.css';
+import { ThemeProvider } from '@material-tailwind/react';
+
+const theme = {
+  button: {
+    defaultProps: {
+      ripple: false,
+    },
+    styles: {
+      base: {
+        initial: {
+          fontWeight: '',
+          textTransform: '',
+        },
+      },
+      sizes: {
+        sm: {
+          fontSize: 'text-xs',
+          py: '',
+          px: '',
+          borderRadius: 'rounded-lg',
+        },
+        md: {
+          fontSize: 'text-xs',
+          py: '',
+          px: '',
+          borderRadius: 'rounded-lg',
+        },
+        lg: {
+          fontSize: 'text-sm',
+          py: '',
+          px: '',
+          borderRadius: 'rounded-lg',
+        },
+      },
+      variants: {
+        filled: {
+          'blue-gray': {
+            background: '',
+            color: 'text-white',
+            active: '',
+          },
+        },
+      },
+    },
+  },
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<Home />);
+root.render(
+  <ThemeProvider value={theme}>
+    <Home />
+  </ThemeProvider>
+);
